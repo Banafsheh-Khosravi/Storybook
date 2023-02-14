1ed8f8d483177d40a61d3d77d6dd803e2ac876ed
Bani <bani.khosravinia@perficient.com>
2023-02-14 15:53:07 -0500

    added lable to the button

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label} Neeeeeew
+      {label} Neeeeeew changes
     </button>
   );
 };
